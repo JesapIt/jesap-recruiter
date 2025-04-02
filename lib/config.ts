92864d23d2506776d56caad296dd047481c5907b
JESAP <119874282+JesapIt@users.noreply.github.com>
2025-04-02 13:55:09 +0300

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
 // Variabile che controlla se il periodo di reclutamento è aperto
-export const isRecruitingSeason = true // Imposta a true per attivare il form di candidatura
+export const isRecruitingSeason = false // Imposta a true per attivare il form di candidatura
